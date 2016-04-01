8cc2088d99fe862ba9337bea051a8fd45d318f8b
Stanislav Kvashchuk <StanislavK.1982@gmail.com>
2016-04-01 22:46:28 +0300

    Commit 1 HomeWork Lesson 4.1

---
+++ src/ua/od/hillel/Lesson4_HW_1.java
@@ -0,0 +1,16 @@
+package ua.od.hillel;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+/**
+ * Created by Стивен on 01.04.2016.
+ */
+public class Lesson4_HW_1 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        for (int i=1000; i<10000; i=i+3) {
+            System.out.println(i);
+        }
+                   //System.out.println(i);
+    }
+}
