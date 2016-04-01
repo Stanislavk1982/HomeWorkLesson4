4224172f794555c7499cb7784c4c88b428f786e6
Stanislav Kvashchuk <StanislavK.1982@gmail.com>
2016-04-01 23:06:48 +0300

    Commit 4 HomeWork Lesson 4.4

---
+++ src/ua/od/hillel/Lesson4_HW_4.java
@@ -0,0 +1,15 @@
+package ua.od.hillel;
+
+/**
+ * Created by Стивен on 01.04.2016.
+ */
+public class Lesson4_HW_4 {
+    public static void main(String[] args) {
+        int number = 2;
+        for (int i = 0; i < 20; i++) {
+            System.out.println(number);
+            number = number * 2;
+        }
+
+    }
+}
